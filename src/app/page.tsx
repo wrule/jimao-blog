5ac8b3dee8f00ee66f3002a96c42e795bbc4e475
jimao <shihao.gu@perfma.com>
2024-10-26 11:37:44 +0800

    还不错的一个markdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import Markdown from 'react-markdown'
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <ConnectButton />
+
+        <Markdown>{`# Hi, *Pluto*!`}</Markdown>
+
         <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
