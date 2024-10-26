e8753340f88a4755116b7460b8cd18bb36003979
jimao <shihao.gu@perfma.com>
2024-10-26 20:46:22 +0800

    提交

--- src/app/api/gif/route.ts
+++ src/app/api/gif/route.ts
@@ -1,23 +1,30 @@
 import { NextResponse } from 'next/server';
-import GIFEncoder from 'gifencoder';
-import { Readable } from 'stream';
+import sharp from 'sharp';
 
 export
 async function POST(request: Request) {
   const formData = await request.formData();
-  // const buffers = await Promise.all(
-  //   (formData.getAll('file') as File[])
-  //     .map(async (file) => Buffer.from(await file.arrayBuffer()))
-  // );
-  // buffers.forEach((buffer) => {
-  //   console.log(buffer.length);
-  // });
-  const encoder = new GIFEncoder(854, 480);
-  const gifStream = encoder.createWriteStream({
-    repeat: -1, delay: 500, quality: 10,
-  });
-  (formData.getAll('file') as File[]).forEach((file) => {
-    Readable.fromWeb(file.stream() as any).pipe(gifStream);
+  const buffers = await Promise.all(
+    (formData.getAll('file') as File[])
+      .map(async (file) => {
+        const arrayBuffer = await file.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+        return await sharp(buffer).resize(300, 150).toBuffer();
+      })
+  );
+  const gifBuffer = await sharp(buffers[0], { animated: true }).gif({ loop: 0, delay: 250 }).composite(
+    buffers.slice(1).map((buffer) => ({
+      input: buffer,
+      top: 0,
+      left: 0,
+      gravity: 'center',
+    }))
+  ).toBuffer();
+  return new Response(gifBuffer, {
+    headers: {
+      'Content-Type': 'image/gif',
+      'Content-Length': gifBuffer.length.toString(),
+    },
   });
   return NextResponse.json({
     message: '你好，世界',
